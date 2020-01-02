3c4c44f59624df5ec415012264c21f67e13c9980
gmirabella <gmirabella@cuebiq.com>
2020-01-02 11:49:49 +0100

    added client docker

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ echo "*******************************"
 echo "  Compile dashboard-download project"
 echo "*******************************"
 
+cd server
 mvn clean install
+cd ..
 
 echo "**********************************"
 echo "  Create dashboard-download dockerImage"
@@ -13,4 +15,3 @@ echo "**********************************"
 
     docker-compose -f docker-compose.yml build
     docker-compose -f docker-compose.yml up
-
